5ba1dde443ebed71d55ada70aeb89e65c03255cd
jejovanni <jamaineekong@gmail.com>
2023-09-09 22:56:56 +0100

    stil on vercel

--- src/main.jsx
+++ src/main.jsx
@@ -57,32 +57,7 @@ const router = createBrowserRouter(
       <Route exact path="/register" element={<Register />} />
       <Route path='/dashboard' element={<Dashboard />} />
     </Route>
-    // [
-    //   {
-    //     path: "/",
-    //     element: <App />,
-    //   },
-    //   {
-    //     path: "dashboard",
-    //     element: <Dashboard />,
-    //   },
-    //   {
-    //     path: "signup",
-    //     element: <Signup />,
-    //   },
-    //   {
-    //     path: "about",
-    //     element: <About />,
-    //   },
-    //   {
-    //     path: "register",
-    //     element: <Register />,
-    //   },
-    //   {
-    //     path: "login",
-    //     element: <Login />,
-    //   },
-    // ]
+    
   ));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
